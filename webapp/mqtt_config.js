fe8569640893ad0ad091c3698ddcca5516baf76f
Akshaya <akshaya.aienavolu@gmail.com>
2025-11-26 13:44:51 +0530

    Fix MQTT WebSocket port to 8884

--- webapp/mqtt_config.js
+++ webapp/mqtt_config.js
@@ -2,7 +2,7 @@
 // WARNING: Do NOT commit `mqtt_config.js` to public repos — it contains secrets.
 
 window.MQTT_CONFIG = {
-  WSS_URL: "wss://308c552d0a56494799306611ffacac19.s1.eu.hivemq.cloud:8883/mqtt",
+  WSS_URL: "wss://308c552d0a56494799306611ffacac19.s1.eu.hivemq.cloud:8884/mqtt",
   USERNAME: "feronmobile",
   // Replace the string below with the password you created in HiveMQ Cloud.
   PASSWORD: "Qwerty123",
