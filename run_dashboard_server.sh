47df9e606c9eea3afd2ae5d4e1d8a17819a9b0ae
Akshaya <akshaya.aienavolu@gmail.com>
2025-12-04 15:04:58 +0530

    Fix database path in startup script

--- run_dashboard_server.sh
+++ run_dashboard_server.sh
@@ -2,8 +2,13 @@
 
 # Dashboard Server Startup Script for Raspberry Pi
 
-# Set database path (default to /home/pi/fer_events.db if not set)
-export FER_DB="${FER_DB:-/home/pi/fer_events.db}"
+# Set database path (default to local fer_events.db in project dir)
+# If running from IOT_Project, use that. Otherwise fallback to home.
+if [ -f "/home/pi/IOT_Project/fer_events.db" ]; then
+    export FER_DB="/home/pi/IOT_Project/fer_events.db"
+else
+    export FER_DB="${FER_DB:-/home/pi/fer_events.db}"
+fi
 
 # Activate virtual environment if exists
 if [ -d "/home/pi/IOT_Project/fer_env" ]; then
